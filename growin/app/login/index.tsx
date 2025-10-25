68648d70692cf781210e57565bb76c1a8f962ce9
ParkSeohyeon <tjgus4394@gmail.com>
2025-10-25 23:03:15 +0900

    feat/#5: growin 로고 및  로그인 버튼 추가

--- growin/app/login/index.tsx
+++ growin/app/login/index.tsx
@@ -1,19 +1,45 @@
 import { router } from 'expo-router';
 import React from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Image, StyleSheet, TouchableOpacity, View } from 'react-native';
 
 export default function LoginScreen() {
-  const handleLogin = () => {
-    // 로그인 버튼을 누르면 탭 화면으로 이동
-    router.replace('/(tabs)');
+  const handleKakaoLogin = () => {
+    // 카카오 로그인 버튼을 누르면 온보딩 화면으로 이동
+    router.replace('/onboarding');
+  };
+
+  const handleAppleLogin = () => {
+    // 애플 로그인 버튼을 누르면 온보딩 화면으로 이동
+    router.replace('/onboarding');
   };
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>로그인 화면</Text>
-      <TouchableOpacity style={styles.button} onPress={handleLogin}>
-        <Text style={styles.buttonText}>로그인</Text>
-      </TouchableOpacity>
+      <View style={styles.logoContainer}>
+        <Image 
+          source={require('../../assets/images/login/growin_login_logo.png')} 
+          style={styles.logo}
+          resizeMode="contain"
+        />
+      </View>
+      
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity onPress={handleKakaoLogin}>
+          <Image 
+            source={require('../../assets/images/login/kakao_login_button.png')} 
+            style={styles.kakaoLoginIcon} 
+            resizeMode="contain" 
+          />
+        </TouchableOpacity>
+        
+        <TouchableOpacity onPress={handleAppleLogin}>
+          <Image 
+            source={require('../../assets/images/login/apple_login_button.png')} 
+            style={styles.appleLoginIcon} 
+            resizeMode="contain" 
+          />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -22,24 +48,32 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#10121F',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingVertical: 60,
+  },
+  logoContainer: {
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
   },
-  text: {
-    color: 'white',
-    fontSize: 24,
-    marginBottom: 40,
+  buttonContainer: {
+    alignItems: 'center',
+    width: '100%',
+    paddingHorizontal: 20,
   },
-  button: {
-    backgroundColor: '#FF008B',
-    paddingHorizontal: 40,
-    paddingVertical: 15,
-    borderRadius: 25,
-    marginTop: 20,
+  logo: {
+    width: 171.81,
+    height: 286.1,
   },
-  buttonText: {
-    color: 'white',
-    fontSize: 18,
-    fontWeight: '600',
+  kakaoLoginIcon: {
+    width: 350,
+    height: 56,
+    marginBottom: 10,
+  },
+  appleLoginIcon: {
+    width: 350,
+    height: 56,
+    marginBottom: 30,
   },
 });
