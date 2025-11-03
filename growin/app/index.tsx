364d83c495600c66995e991ddab718e6e897372c
ion <delion.dev@gmail.com>
2025-11-03 09:51:00 +0900

    feat/#7: 홈 화면에 일정 카드 추가 및 투두 항목 렌더링 구현

--- growin/app/index.tsx
+++ growin/app/index.tsx
@@ -1,9 +1,14 @@
-import { StyleSheet, Text, View } from "react-native";
+import ScheduleCard from "@/components/ScheduleCard";
+import { StyleSheet, View } from "react-native";
 
 export default function HomeScreen() {
+  const todos = [
+    { id: 0, text: "동역사 스타벅스 가기", done: false },
+    { id: 1, text: "노트북 환경 설정", done: true },
+  ];
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>홈</Text>
+      <ScheduleCard id={0} title="FE 회의" time="16:00 - 18:00" todos={todos} />
     </View>
   );
 }
