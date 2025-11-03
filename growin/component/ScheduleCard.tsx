09cf18a321b38335c2b9400a42ca4d4b33ab1a30
ion <delion.dev@gmail.com>
2025-11-03 09:17:25 +0900

    feat/#7: 일정 투두 항목 및 체크 UI 구현

--- growin/component/ScheduleCard.tsx
+++ growin/component/ScheduleCard.tsx
@@ -76,6 +76,95 @@ export default function ScheduleCard() {
           />
         </TouchableOpacity>
       </View>
+
+      {/* 일정 투두 */}
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          width: "100%",
+          justifyContent: "space-between",
+          paddingTop: 6.5,
+          borderTopWidth: 1,
+          borderTopColor: "#ffffff1a",
+        }}
+      >
+        <Text
+          style={{
+            color: "#fff",
+            fontSize: 13,
+            fontWeight: 500,
+            lineHeight: 18.9241,
+          }}
+        >
+          회의록 작성
+        </Text>
+
+        {/* 체크 비활성 */}
+        <TouchableOpacity>
+          <View
+            style={{
+              width: 20,
+              height: 20,
+              borderRadius: 10,
+              borderWidth: 2,
+              borderColor: "#3F4360",
+            }}
+          />
+        </TouchableOpacity>
+      </View>
+
+      {/* 일정 투두 */}
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          width: "100%",
+          justifyContent: "space-between",
+          paddingTop: 6.5,
+          borderTopWidth: 1,
+          borderTopColor: "#ffffff1a",
+        }}
+      >
+        <Text
+          style={{
+            color: "#8E8E93",
+            fontSize: 13,
+            fontWeight: 500,
+            lineHeight: 18.9241,
+          }}
+        >
+          회의록 작성
+        </Text>
+
+        {/* 체크 활성 */}
+        <TouchableOpacity>
+          <View
+            style={{
+              width: 20,
+              height: 20,
+              flexShrink: 0,
+              aspectRatio: 1 / 1,
+              borderWidth: 2,
+              borderRadius: 10,
+              borderColor: "#FF008B",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <View
+              style={{
+                width: 12,
+                height: 12,
+                flexShrink: 0,
+                aspectRatio: 1 / 1,
+                borderRadius: 6,
+                backgroundColor: "#FF008B",
+              }}
+            />
+          </View>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
