5a9ad3ad3a9b739ec0799f4376356763a101851e
ion <delion.dev@gmail.com>
2025-11-07 12:25:45 +0900

    feat/#15: DayGrid 컴포넌트 및 DayGridType 타입 추가

---
+++ growin/components/calendar/DayGrid.tsx
@@ -0,0 +1,131 @@
+import { events } from "@/mock/calendarEvent";
+import { DayGridType } from "@/types/calendar";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { StyleSheet, Text, View } from "react-native";
+
+interface DayGridProps {
+  dayIndex: number;
+  isBorderRight: boolean;
+  isToday: boolean;
+  day: DayGridType;
+}
+
+const DayGrid = ({ dayIndex, isBorderRight, isToday, day }: DayGridProps) => {
+  return (
+    <View
+      key={dayIndex}
+      style={[styles.dayCell, isBorderRight && styles.borderRight]}
+    >
+      {/* 일자 */}
+      <View style={styles.dayHeader}>
+        {/* 오늘 표시 */}
+        {isToday && <View style={styles.todayIndicator} />}
+
+        <View style={styles.dayNumberContainer}>
+          {/* 숫자 */}
+          <Text
+            style={[
+              styles.dayNumber,
+              !day.isCurrentMonth && styles.inactiveDay,
+            ]}
+          >
+            {day.day}
+          </Text>
+
+          {/* 할 일 4개 이상 */}
+          {day.isCurrentMonth &&
+            events[day.day] &&
+            events[day.day].length > 3 && (
+              <Ionicons name="add" size={6} color="white" />
+            )}
+        </View>
+      </View>
+
+      {/* 할 일 목록 */}
+      {day.isCurrentMonth &&
+        events[day.day] &&
+        events[day.day].slice(0, 3).map((eventItem, eventIndex) => (
+          <View
+            key={eventIndex}
+            style={[styles.eventTag, { backgroundColor: eventItem.color }]}
+          >
+            <Text style={styles.eventText} numberOfLines={1}>
+              {eventItem.title}
+            </Text>
+          </View>
+        ))}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  dayCell: {
+    flex: 1,
+    height: 56,
+    paddingVertical: 2,
+    alignItems: "center",
+    gap: 2,
+    flexShrink: 0,
+  },
+  borderRight: {
+    borderRightWidth: 1,
+    borderRightColor: "#3F4360",
+  },
+  dayHeader: {
+    width: "100%",
+    position: "relative",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  todayIndicator: {
+    position: "absolute",
+    width: 20,
+    height: 13,
+    flexShrink: 0,
+    backgroundColor: "#FF008B",
+    borderRadius: 15,
+    alignSelf: "center",
+  },
+  dayNumberContainer: {
+    width: 40,
+    position: "relative",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  dayNumber: {
+    position: "relative",
+    alignItems: "center",
+    color: "#FFFFFF",
+    textAlign: "center",
+    fontSize: 10,
+    fontWeight: "500",
+    lineHeight: 13,
+  },
+  inactiveDay: {
+    color: "#8E8E93",
+  },
+  plusIcon: {
+    position: "absolute",
+    flexShrink: 0,
+    right: 0,
+  },
+  eventTag: {
+    flexDirection: "row",
+    minWidth: "100%",
+    // paddingVertical: 1,
+    paddingHorizontal: 2,
+    alignItems: "center",
+    flexShrink: 0,
+    borderRadius: 3,
+    height: 11,
+  },
+  eventText: {
+    flexShrink: 0,
+    overflow: "hidden",
+    color: "#FFFFFF",
+    fontSize: 8,
+    lineHeight: 10.4,
+  },
+});
+
+export default DayGrid;
