28ef9edd3b934919a66ae7a26065f481648a2a28
ParkSeohyeon <tjgus4394@gmail.com>
2025-12-11 00:46:46 +0900

    feat/#34: 메인화면 리스트 카테고리 컴포넌트 추가

--- TinyBite/app/(tabs)/index.tsx
+++ TinyBite/app/(tabs)/index.tsx
@@ -1,3 +1,4 @@
+import MainCategory from "@/components/mainCategory";
 import MainHeader from "@/components/mainHeader";
 import MainList from "@/components/mainList";
 import { ScrollView, StyleSheet, View } from "react-native";
@@ -6,6 +7,7 @@ export default function HomeScreen() {
   return (
     <ScrollView style={styles.scroll} contentContainerStyle={styles.screen}>
       <MainHeader />
+      <MainCategory />
       <View style={styles.listWrapper}>
         <MainList />
       </View>
@@ -20,9 +22,10 @@ const styles = StyleSheet.create({
   },
   screen: {
     alignItems: "center",
+    gap: 8,
   },
   listWrapper: {
-    marginTop: 8,
+    marginTop: 4,
   },
 });
 
