d85e3ce3298c16d78558089d6810bb8224d119e5
ion <delion.dev@gmail.com>
2025-12-11 15:06:34 +0900

    feat/#32: 로그인 화면으로 리디렉션 경로 수정

--- TinyBite/app/_layout.tsx
+++ TinyBite/app/_layout.tsx
@@ -1,9 +1,5 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="(tabs)" />
-    </Stack>
-  );
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
