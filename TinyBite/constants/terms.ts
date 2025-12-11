4bdd95f4395770d195b6f28137691970562bf784
ion <delion.dev@gmail.com>
2025-12-11 16:50:32 +0900

    feat/#32: 회원가입 약관 상수 추가

---
+++ TinyBite/constants/terms.ts
@@ -0,0 +1,32 @@
+export const SignupTerms = [
+  {
+    checkKey: "age",
+    required: true,
+    content: "만 14세 이상입니다.",
+  },
+  {
+    checkKey: "service",
+    required: true,
+    content: "서비스 이용약관 동의",
+  },
+  {
+    checkKey: "finance",
+    required: true,
+    content: "전자 금융거래 이용약관 동의",
+  },
+  {
+    checkKey: "collectingPrivacy",
+    required: true,
+    content: "개인정보 수집 이용 동의",
+  },
+  {
+    checkKey: "providingPrivacy",
+    required: true,
+    content: "개인정보 제공 동의",
+  },
+  {
+    checkKey: "offer",
+    required: false,
+    content: "다양한 쇼핑정보 및 혜택 수신 동의",
+  },
+];
