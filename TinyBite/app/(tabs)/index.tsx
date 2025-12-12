2b34157187fc72ebf08ef83009cf797db753a114
ParkSeohyeon <tjgus4394@gmail.com>
2025-12-12 13:38:48 +0900

    refactor/#34: 하단 safeArea 삭제 및 useSafeArea 삭제

--- TinyBite/app/(tabs)/index.tsx
+++ TinyBite/app/(tabs)/index.tsx
@@ -2,21 +2,19 @@ import MainCard from "@/components/mainCard";
 import MainCategory from "@/components/mainCategory";
 import MainHeader from "@/components/mainHeader";
 import { ScrollView, StyleSheet, View } from "react-native";
-import { SafeAreaView, useSafeAreaInsets } from "react-native-safe-area-context";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
-  const insets = useSafeAreaInsets();
-  
   return (
-    <View style={styles.wrapper}>
-      <SafeAreaView style={styles.safeAreaTop} edges={["top"]}>
-        <View style={styles.container}>
-          <MainHeader />
-          <MainCategory />
-          <ScrollView 
-            style={styles.scroll}
-            contentContainerStyle={[styles.listWrapper]}
-          ><View style={styles.cardWrapper}>
+    <SafeAreaView style={styles.safeArea} edges={["top"]}>
+      <View style={styles.container}>
+        <MainHeader />
+        <MainCategory />
+        <ScrollView 
+          style={styles.scroll}
+          contentContainerStyle={styles.listWrapper}
+        >
+          <View style={styles.cardWrapper}>
             <MainCard />
             <MainCard />
             <MainCard />
@@ -24,22 +22,14 @@ export default function HomeScreen() {
             <MainCard />
             <MainCard />
           </View>
-           
-          </ScrollView>
-        </View>
-      </SafeAreaView>
-      <View style={[styles.safeAreaBottom, { paddingBottom: Math.max(insets.bottom * 0.01) }]}>
-        <View style={styles.bottomArea} />
+        </ScrollView>
       </View>
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  wrapper: {
-    flex: 1,
-  },
-  safeAreaTop: {
+  safeArea: {
     flex: 1,
     backgroundColor: "#FE870F",
   },
@@ -58,13 +48,6 @@ const styles = StyleSheet.create({
   cardWrapper: {
     gap: 16,
     marginBottom: 16,
-  },
-  safeAreaBottom: {
-    backgroundColor: "#FCFBFF",
-  },
-  bottomArea: {
-    height: 1,
-    backgroundColor: "#FCFBFF",
   },
 });
 
